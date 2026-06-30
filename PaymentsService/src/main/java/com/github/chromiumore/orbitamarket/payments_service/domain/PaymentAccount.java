009d394504be9bb45948c04e6af30b06aa5d2733
Chromiumore <parts.dima06@gmail.com>
2026-06-30 17:51:39 +0300

    database connection and models

---
+++ PaymentsService/src/main/java/com/github/chromiumore/orbitamarket/payments_service/domain/PaymentAccount.java
@@ -0,0 +1,32 @@
+package com.github.chromiumore.orbitamarket.payments_service.domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "payment_accounts")
+@Getter
+@Setter
+@NoArgsConstructor
+public class PaymentAccount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id", nullable = false)
+    private UUID userId;
+
+    @Column(nullable = false)
+    private Double amount = 0.0;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private Instant createdAt;
+
+    @Version
+    private Long version;
+}
