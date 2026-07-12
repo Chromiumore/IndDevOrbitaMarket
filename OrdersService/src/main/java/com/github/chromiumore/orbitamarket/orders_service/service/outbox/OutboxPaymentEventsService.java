9ddf7f806fc7974a47f5ad42bf5026b09396ae16
Chromiumore <parts.dima06@gmail.com>
2026-07-12 18:27:47 +0300

    pending payment status used

--- OrdersService/src/main/java/com/github/chromiumore/orbitamarket/orders_service/service/outbox/OutboxPaymentEventsService.java
+++ OrdersService/src/main/java/com/github/chromiumore/orbitamarket/orders_service/service/outbox/OutboxPaymentEventsService.java
@@ -2,9 +2,11 @@ package com.github.chromiumore.orbitamarket.orders_service.service.outbox;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.chromiumore.orbitamarket.orders_service.domain.order.Order;
+import com.github.chromiumore.orbitamarket.orders_service.domain.order.OrderStatus;
 import com.github.chromiumore.orbitamarket.orders_service.domain.outbox.OutboxPaymentEvent;
 import com.github.chromiumore.orbitamarket.orders_service.dto.event.PaymentRequestedEvent;
 import com.github.chromiumore.orbitamarket.orders_service.kafka.producer.PaymentEventProducer;
+import com.github.chromiumore.orbitamarket.orders_service.repository.OrderRepository;
 import com.github.chromiumore.orbitamarket.orders_service.repository.outbox.OutboxPaymentEventsRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -12,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import javax.security.auth.login.AccountNotFoundException;
 import java.time.Instant;
 import java.util.List;
 
@@ -21,6 +24,7 @@ import java.util.List;
 public class OutboxPaymentEventsService {
 
     private final OutboxPaymentEventsRepository outboxRepository;
+    private final OrderRepository orderRepository;
     private final PaymentEventProducer producer;
     private final ObjectMapper objectMapper;
 
@@ -49,13 +53,22 @@ public class OutboxPaymentEventsService {
         for (OutboxPaymentEvent event : outboxEvents) {
             try {
                 PaymentRequestedEvent paymentEvent = objectMapper.readValue(event.getPayload(), PaymentRequestedEvent.class);
-                producer.sendToKafka(paymentEvent);
 
                 event.setSentAt(Instant.now());
                 outboxRepository.save(event);
+
+                Order order = orderRepository.findById(event.getAggregateId()).orElseThrow(
+                        () -> new AccountNotFoundException("Account not found")
+                );
+                order.setStatus(OrderStatus.PAYMENT_PENDING);
+                orderRepository.save(order);
+
+                producer.sendToKafka(paymentEvent);
+
                 log.info("Outbox event {} sent to Kafka", event.getId());
             } catch (Exception e) {
-                log.error("Failed to publish outbox event {}", event.getId(), e);
+                log.error("Failed to publish outbox event {}", event.getId());
+                throw new RuntimeException("Failed to publish outbox event");
             }
         }
     }
